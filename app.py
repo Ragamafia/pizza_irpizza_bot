bdb7de60e4b77af547e8313e3e5c91a69b440a30
ragamafia <pragim.atat@mail.ru>
2024-04-12 18:50:09 +0800

    lesson 3

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 
-bot = Bot(token='6996415512:AAFNgePCqxbPKFD4qoSBtTHpaqoopmkolWg')
+from handlers.user_private import user_private_router
 
+ALLOWED_UPDATES = ['message, edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message) -> None:
-    await message.answer('Была команда Старт')
+dp.include_router(user_private_router)
 
 
-@dp.message()
-async def echo(message: types.Message) -> None:
-    text: str | None = message.text
-
-    if text in ['привет', 'Привет', 'hi', 'hello']:
-        await message.answer('И тебе привет!')
-    elif text in ['пока', 'Пока', 'by']:
-        await message.answer('И тебе пока!')
-    else:
-        await message.answer(message.text)
-
-
-async def main() -> None:
-    await dp.start_polling(bot)
-
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
